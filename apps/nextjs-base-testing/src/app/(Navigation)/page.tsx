2815d3c13d1c4934bf22b253798a30b788d57648
Daniel Craciun <danielcraciun111@yahoo.com>
2024-07-12 13:12:04 +0100

    undo creation of base testing template in monorepo

--- apps/nextjs-base-testing/src/app/(Navigation)/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Home() {
-  return (
-    <section className="flex flex-col">
-      <div className="text-4xl font-extrabold tracking-tight md:text-5xl">
-        Hello world
-      </div>
-    </section>
-  )
-}
